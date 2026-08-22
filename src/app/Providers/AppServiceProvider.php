0945bf2f37ff972e3a22aafba4d430d384e31959
excelsior091224 <44745901+excelsior091224@users.noreply.github.com>
2026-08-22 17:10:49 +0000

    feat: Viteのホットファイル設定を追加

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -22,6 +22,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Vite::useHotFile(storage_path('framework/vite.hot'));
         Vite::createAssetPathsUsing(
             fn(string $path): string => '/' . ltrim($path, '/')
         );
